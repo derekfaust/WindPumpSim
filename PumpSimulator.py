d97c4959a867063f26cb3c0a09dbd9e7afccf88c
Derek Faust <faust.df@gmail.com>
2013-03-18 16:14:16 -0400

    New paramter input format

--- PumpSimulator.py
+++ PumpSimulator.py
@@ -9,13 +9,36 @@ from math import *
 import statepredictor as sp
 from pump import *
 
+#Simulation Parameters
+timespan = [0,100]
+timesteps = 1000
+initial_theta= 0
+initial_omega= 20
+
+#Turbine Parameters
+turbine_I = 1
+omega_torque_curve = np.array([[0,  0],
+                               [20, 5],
+                               [40, 4],
+                               [60, 3],
+                               [80, 2],
+                               [100,1],
+                               [120,0]])
+#Piston Parameters
+crank_radius = .03
+rod_length = .3
+piston_radius = .03
+piston_mass = .1
+initial_piston_angle = 0
+
 #Initial Conditions
-times = np.linspace(0,100,1000)
-initial_state = np.array([0,20])
+times = np.linspace(timespan[0],timespan[1],timesteps)
+initial_state = np.array([initial_theta,initial_omega])
 
 #Create Objects to Simulate
-the_turbine = Turbine(1, np.array([[0,0],[20,5],[40,4],[60,3],[80,2],[100,1]]))
-one_piston = Piston(.03, .3, .03, .1, 0)
+the_turbine = Turbine(turbine_I, omega_torque_curve)
+one_piston = Piston(crank_radius, rod_length, piston_radius, piston_mass,
+                    initial_piston_angle)
 the_pump = Pump(the_turbine, one_piston)
 
 states = sp.predict(the_pump.statedot, times, initial_state, [])
