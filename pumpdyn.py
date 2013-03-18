7bab6c7f8fafea379019c66a65ca7fbddda55c4b
Derek Faust <faust.df@gmail.com>
2013-03-18 09:37:24 -0400

    pumpdyn ODE requests omegadot from pump. No calculation done in ODE method

--- pumpdyn.py
+++ pumpdyn.py
@@ -1,7 +1,6 @@
 #Import required modules
 from __future__ import division
 import numpy as np
-import pylab as plot
 from math import *
 from scipy.integrate import ode
 
@@ -19,21 +18,10 @@ def solveODE(f, tarray, z0, p):
         zarray[idx,:] = eqn.y
     return zarray
 
-def rhs(t, z, turbine):
-    theta = z[0]
-    omega = z[1]
-    KE = .5 * omega**2 * turbine.I
-    #Need to return omegadot. How??
-    turbine.set_state(t, [theta, omega, omegadot])
-    P = (turbine.Tin(omega)-turbine.Tout(t))*omega
-    #print p.Tin(omega)
-    #print t, omega, p.Tin(omega)
-    if (KE <= 0) and (P<=0):
-        zdot = np.array([-omega,-KE])
-    elif (KE <=0):
-        zdot = np.array([-omega,P])
-    else:
-        zdot = np.array([sqrt(2*KE/p.Idrive)-omega,P])
-    turbine.set_state(
+def rhs(t, z, pump):
+    state = z
+    thetadot = omega
+    omegadot = pump.omegadot(state)
+    zdot = [thetadot, omega]
     return zdot
 
