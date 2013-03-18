de6d136f4a5625a41b19c52814eb7a3753c9bb7a
Derek Faust <faust.df@gmail.com>
2013-03-18 15:14:32 -0400

    removed print statements

--- statepredictor.py
+++ statepredictor.py
@@ -5,9 +5,6 @@ from math import *
 from scipy.integrate import ode
 
 def predict(statedot, times, initial_state, parameters):
-    print np.size(times)
-    print np.size(initial_state)
-    print initial_state
     state_array = np.zeros((np.size(times), np.size(initial_state)))
     eqn = ode(statedot).set_integrator('vode')
     eqn.set_initial_value(initial_state, times[0]).set_f_params(parameters)
